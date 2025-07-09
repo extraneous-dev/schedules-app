d0d43868e9085845821ff683f1fd06294e775baa
Jose Sanchez <jsanchez@vailsys.com>
2025-07-09 18:33:02 -0500

    a

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/libsql';
-
-export const db = drizzle(process.env.DB_FILE_NAME!);
+drizzle({
+    
+})
+export const db = drizzle(process.env.DATABASE_URL!);
