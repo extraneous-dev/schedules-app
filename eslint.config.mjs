2db80c29eb515075f99c84ad9790652c859d9f0c
Jose Sanchez <jsanchez@vailsys.com>
2025-07-09 15:04:35 -0500

    changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,16 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  // ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next"],
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-page-custom-font": "off",
+      "unused-imports/no-unused-imports-ts": "off",
+      "@typescript-eslint/no-unused-vars": "off"
+    },
+  }),
 ];
 
 export default eslintConfig;
