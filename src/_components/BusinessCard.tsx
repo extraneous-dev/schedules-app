1a0b2e4d9852e631d497c1714cd3984e5d64af8e
Jose Sanchez <jsanchez@vailsys.com>
2025-07-09 15:34:01 -0500

    add tyes for BusinessCard

--- src/_components/BusinessCard.tsx
+++ src/_components/BusinessCard.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link"
-
-export const BusinessCard = ({title, id}) => {
+interface BusinessCardProps {
+    title: string;
+    id: string;
+}
+export const BusinessCard = ({title, id}: BusinessCardProps) => {
     return(
         <div className="bg-white w-96 h-96 rounded-2xl p-4 box-border">
-            <h2 className="text-black font-bold text-3xl">The best deals on labubus and much more.</h2>
+            <h2 className="text-black font-bold text-3xlgi">The best deals on labubus and much more.</h2>
             <h1 className="text-red-500 text-xl">{title}</h1>
             <Link href={`/business/${id}`} className="text-red-500 hover:text-white hover:bg-red-500">Explore Business</Link>
         </div>
